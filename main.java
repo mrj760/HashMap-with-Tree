0ad81c08a17be66806919900ab39aea8bced95ad
Micah Johnson <mj2carlsbad@gmail.com>
2023-07-04 20:38:44 -0700

    fixed insert() bug. added contains(). added size(). made HashMap funcs public.

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 import java.util.List;
 
 class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         HashMap<Integer, String> map = new HashMap<>(3);
 
         pl(map.insert(4, "four"));
@@ -11,6 +11,14 @@ class Main {
 
         pl(map.insert(4, "five"));
         pl(map.get(4));
+
+        pl(map.insert(2, "two"));
+        pl(map.get(2));
+
+        pl(map.insert(7, "seven"));
+        pl(map.get(7));
+
+        pl(map.size());
     }
 
     public static void pl(Object o) {
@@ -27,16 +35,19 @@ class HashMap<K extends Comparable<K>, V> implements Comparator<K> {
             Node right;
             K key;
             V val;
+
             Node(K key, V val) {
                 this.key = key;
                 this.val = val;
             }
         }
-    
+
         Node head;
-        Tree() {}
-    
-        Tree (K key, V val) {
+
+        Tree() {
+        }
+
+        Tree(K key, V val) {
             head = new Node(key, val);
         }
 
@@ -48,28 +59,53 @@ class HashMap<K extends Comparable<K>, V> implements Comparator<K> {
             }
             Node cur = head;
             while (true) {
-                int comp = key.compareTo(key);
+                int comp = key.compareTo(cur.key);
                 if (comp < 0) {
                     if (cur.left == null) {
                         cur.left = new Node(key, val);
                         break;
                     }
                     cur = cur.left;
-                }
-                else if (comp > 0) {
+                } else if (comp > 0) {
                     if (cur.right == null) {
                         cur.right = new Node(key, val);
                         break;
                     }
                     cur = cur.right;
-                }
-                else {
+                } else {
                     return false;
                 }
             }
             return true;
         }
-    
+
+        boolean contains(K key) {
+            Node cur = head;
+            while (true) {
+                if (head == null) {
+                    return false;
+                }
+                int comp = key.compareTo(cur.key);
+                if (comp < 0) {
+                    cur = cur.left;
+                } else if (comp > 0) {
+                    cur = cur.right;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        // int size() {
+        //     return size(head);
+        // }
+
+        // private int size(Node node) {
+        //     if (node == null)
+        //         return 0;
+        //     return 1 + size(node.left) + size(node.right);
+        // }
+
         // return null if does not exist
         V get(K key) {
             Node cur = head;
@@ -80,11 +116,9 @@ class HashMap<K extends Comparable<K>, V> implements Comparator<K> {
                 int comp = key.compareTo(cur.key);
                 if (comp < 0) {
                     cur = cur.left;
-                }
-                else if (comp > 0) {
+                } else if (comp > 0) {
                     cur = cur.right;
-                }
-                else {
+                } else {
                     return cur.val;
                 }
             }
@@ -92,27 +126,42 @@ class HashMap<K extends Comparable<K>, V> implements Comparator<K> {
     }
 
     private List<Tree> trees;
-    final int size;
+    final int hashSlots;
+    int size = 0;
 
-    HashMap (int size) {
-        if (size < 1) throw new IllegalArgumentException("Size must be greater than 0");
-        this.size = size;
-        trees = new ArrayList<Tree>(size);
-        for (int i = 0; i < size; ++i) {
+    HashMap(int hashSlots) {
+        if (hashSlots < 1)
+            throw new IllegalArgumentException("Size must be greater than 0");
+        this.hashSlots = hashSlots;
+        trees = new ArrayList<Tree>(hashSlots);
+        for (int i = 0; i < hashSlots; ++i) {
             trees.add(new Tree());
         }
     }
 
-    boolean insert(K key, V val) {
-        return trees.get(key.hashCode() % size).insert(key, val);
+    public boolean insert(K key, V val) {
+        if (trees.get(key.hashCode() % hashSlots).insert(key, val)) {
+            size++;
+            return true;
+        }
+        return false;
     }
-    
-    V get(K key) {
-        return trees.get(key.hashCode() % size).get(key);
+
+    public V get(K key) {
+        return trees.get(key.hashCode() % hashSlots).get(key);
     }
 
     @Override
-        public int compare(K key1, K key2) {
-            return key1.compareTo(key2);
-        }
+    public int compare(K key1, K key2) {
+        return key1.compareTo(key2);
+    }
+
+    public int size() {
+        // int size = 0;
+        // for (Tree tree : trees) {
+        //     size += tree.size();
+        // }
+        // return size;
+        return this.size;
+    }
 }
